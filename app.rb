33ece0a22e4bcb601265621b364f41a0bd6a3604
Nataliya R <n.rudenko@gmail.com>
2019-05-27 01:48:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,6 +5,13 @@ get '/team' do
   erb :newteam
   end 
   post '/team' do 
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
     erb :team 
   end
 
